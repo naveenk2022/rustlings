9f70f92f434294b953dfb0f250cb692025fdece3
naveenk2022 <naveen.kannan12@gmail.com>
2025-09-25 10:48:12 -0400

    Completed functions

--- exercises/01_variables/variables4.rs
+++ exercises/01_variables/variables4.rs
@@ -1,10 +1,14 @@
 // TODO: Fix the compiler error.
 fn main() {
     //let x = 3;
+
     // Making the variable mutable
     let mut x = 3;
     println!("Number {x}");
 
+
     x = 5; // Don't change this line
+    // If we can't change this line, then x must be made mutable
+    // Otherwise, the line should be preceeded by `mut`
     println!("Number {x}");
 }
--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,9 @@ fn main() {
     println!("Spell a number: {}", number);
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    //number = 3;
+
+    //Fixing by adding `let` to the statement
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
